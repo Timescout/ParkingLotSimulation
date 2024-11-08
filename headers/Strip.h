1dd5c2715f6da3f2fd6596181ff061db33917f35
Timescout <timepolicescout@gmail.com>
2024-11-08 10:14:57 -0800

    moved parking spot struct form this file.

--- headers/Strip.h
+++ headers/Strip.h
@@ -9,34 +9,10 @@
 #include <Utilities.h>
 
 #include <vector>
+#include <Car.h> // need to know how fast Cars go.
 //#include <gtest/gtest.h>
 
-struct ParkingSpot
-{
-    bool occupied;
-    TimePoint nextVacantTime;
-    Distance distanceFromEnterence1;
-    Distance distanceFromEnterence2;
 
-    ParkingSpot();
-
-    /// @brief Tells this Spot that a Car has parked.
-    /// @param nextVacant The next time point when this spot will be vacant.
-    void park(TimePoint nextVacant);
-
-    /// @brief Tells this Spot that the car parked here has left.
-    void depart();
-
-    static constexpr bool defaultOccupied = false;
-    static constexpr TimePoint defaultNextVacantTime = 0;
-    static constexpr Distance defaultDistanceFromEnterence = 0;
-    static constexpr int parkingSpotWidth = 8;
-};
-
-// need to include Car.h here to avoid inclusion errors
-#include <Car.h> // need to know how fast Cars go.
-
-// Note parking spots are 8 feet by 18 feet. 
 
 /// @brief A road that has parking spots along the sides of it.
 class Strip
